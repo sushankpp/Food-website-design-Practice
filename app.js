46468dcfb3e07c538b76a6116dfbe5472e7b234f
Sushank Pandey <sushankpandey233@gmail.com>
2023-01-21 18:37:24 +0545

    origin

---
+++ app.js
@@ -0,0 +1,13 @@
+const button = document.getElementById("button");
+const btn = document.getElementById("btn");
+
+button.addEventListener('click', event =>{
+    alert('You dont have that facility big guy!');
+    console.log('you clicked')
+});
+
+
+btn.addEventListener('click', event =>{
+    alert('You dont have that facility big guy!');
+    console.log('you clicked')
+});
